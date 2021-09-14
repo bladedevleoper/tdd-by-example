c2f6bc570c404643f45201a04bbf5966a12c6f1f
James Jones <jamesjonestred@gmail.com>
2021-09-14 23:14:13 +0000

    test

--- git.sh
+++ git.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #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
 user="bladedevleoper";
-pass=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;
+pass="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";
 
 
 git add .
 echo "what is your commit message"
 read commitMessage
 git commit -m "$commitMessage"
-git push "https://${user}:${pass}@github.com/$user/tdd-by-example.git" --all
+git push "https://$user:$pass@github.com/$user/tdd-by-example.git" --all
 
