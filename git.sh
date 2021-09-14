4c6694d9bd846bf9508f10f3fa303464c9e755f6
James Jones <jamesjonestred@gmail.com>
2021-09-14 22:48:03 +0000

    just a test

---
+++ git.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#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
+echo "git adding"
+user="bladedevleoper";
+pass="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";
+
+
+git add .
+read commitMessage
+echo "what is your commit message"
+git commit -m "$commitMessage"
+
+git push
+
